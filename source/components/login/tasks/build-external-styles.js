8eb50f27204648ad3cb97d098479446e3b21d9cf
Diegoni <romain_diegoni@hotmail.com>
2016-12-19 12:28:20 +0000

    WIP: clean + add debug + simplify code

--- source/components/login/tasks/build-external-styles.js
+++ source/components/login/tasks/build-external-styles.js
@@ -1,37 +1,27 @@
 'use strict';
 
 var argv = require('yargs').argv;
-var path = require('path');
-var mergeStream = require('merge-stream');
 var mainBowerFiles = require('main-bower-files');
+var buildHelper = require('../buildHelper')();
 
 module.exports = function(gulp, plugins, config, options) {
   return function() {
-    function getSources(root) {
-      if (root instanceof Array) {
-        return root
-          .map(function(source) {
-            return gulp.src(mainBowerFiles({
-                  paths: source,
-                  filter: '**/*.{css,scss}',
-                  overrides: config.bowerOverrides
-                }), { base: './' });
-          });
-      }
 
-      return gulp.src(mainBowerFiles({
-            paths: root,
-            filter: '**/*.{css,scss}',
-            overrides: config.bowerOverrides
-          }), { base: './' });
-    };
+    var sources = buildHelper.getSources(gulp, config.source, function(root) {
+      return mainBowerFiles({
+        paths: root,
+        filter: '**/*.{css,scss}',
+        overrides: config.bowerOverrides
+      });
+    }, { base: './' });
 
-    var sources = getSources(config.source);
-    return (sources instanceof Array ? mergeStream(sources) : sources)
+    return sources
         .pipe(plugins.sass())
         .pipe(plugins.if(argv.production, plugins.csso()))
         .pipe(plugins.flatten())
         .pipe(plugins.mainDedupe({ same: false }))
+        .pipe(plugins.if(argv.debug, plugins.debug({ title: "build-external-styles" })))
         .pipe(gulp.dest(config.destination));
+
   };
 };
